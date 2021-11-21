5beabe10067903d426fd9fab31dc6e71d09c36c1
Xubai Wang <self@xubai.wang>
2021-11-21 18:35:49 +0800

    fix: add c target

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,39 @@
 import Lake
-open Lake DSL
+open System Lake DSL
 
-package terminal {
+def nativeOTarget (pkgDir : FilePath) : FileTarget :=
+  oFileTarget (pkgDir / "./build/native/native.o") (pkgDir / "./native/native.c" : FilePath) #[] "leanc"
+
+def cLibTarget (pkgDir : FilePath) : FileTarget :=
+  staticLibTarget (pkgDir / "./build/native/native.a") #[nativeOTarget pkgDir]
+
+package terminal (pkgDir) (args) {
+  moreLibTargets := #[cLibTarget pkgDir]
   defaultFacet := PackageFacet.staticLib
 }
+
+script examples (args) do
+  let examplesDir ← ("examples" : FilePath).readDir
+  for ex in examplesDir do
+    IO.println ex.path
+    let o ← IO.Process.output {
+      cmd := "lake"
+      args := #["build"]
+      cwd := ex.path
+    }
+    IO.println o.stderr
+  return 0
+
+script clean (args) do
+  let examplesDir ← ("examples" : FilePath).readDir
+  let o ← IO.Process.output {
+      cmd := "lake"
+      args := #["clean"]
+  }
+  for ex in examplesDir do
+    let o ← IO.Process.output {
+      cmd := "lake"
+      args := #["clean"]
+      cwd := ex.path
+    }
+  return 0
